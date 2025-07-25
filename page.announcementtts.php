0f8c556fe054ecb75d07a03c5478d264ab84148b
maladrill <maladrill@users.noreply.github.com>
2025-07-25 20:06:40 +0200

    Update page.announcementtts.php

--- page.announcementtts.php
+++ page.announcementtts.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 // License for all code of this FreePBX module can be found in the license file inside the module directory
-// Copyright 2006-2014 Schmooze Com Inc.
+// Copyright 2006-2014 Schmooze Com Inc. and 2025 SQS Polska
 
 $request = $_REQUEST;
 
