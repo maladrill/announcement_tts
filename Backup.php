53890678b007636b73c4b93eeb1d5ff113ad8c64
maladrill <maladrill@users.noreply.github.com>
2025-08-01 12:03:22 +0200

    Update Backup.php

--- Backup.php
+++ Backup.php
@@ -2,6 +2,7 @@
 namespace FreePBX\modules\Announcementtts;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
+ 
   public function runBackup($id,$transaction){
     $this->addDependency('Core');
     $this->addDependency('Recordings');
