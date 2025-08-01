62697bc4dd8bbd4b57cb3ae129828708995bc012
maladrill <maladrill@users.noreply.github.com>
2025-08-01 12:10:12 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,4 +2,3 @@
 //	License for all code of this FreePBX module can be found in the license file inside the module directory
 //	Copyright 2015 Sangoma Technologies.
 // vim: set ai ts=4 sw=4 ft=php:
-
